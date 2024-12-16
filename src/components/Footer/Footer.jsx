ba1732bffc6bd5814382eae6c0e6d2ecc393705a
BrandonRogers <ber64111@gmail.com>
2024-12-16 00:59:14 -0700

    768px and 320px media queries

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,10 @@ function Footer() {
         <footer className="footer">
             <p className="footer__details">&copy;2024 Supersite, Powered by News API</p>
             <div className="footer__links">
-                <a href="/" className="footer__home-link">Home</a>
-                <a href="https://tripleten.com/" className="footer__tripleten-link">TripleTen</a>
+                <div className="footer__navigation-links">
+                    <a href="/" className="footer__home-link">Home</a>
+                    <a href="https://tripleten.com/" className="footer__tripleten-link">TripleTen</a>
+                </div>
                 <a href="https://github.com/tripleten-com" className="footer__github-link"></a>
                 <a href="https://www.facebook.com/tripleten.tech/" className="footer__facebook-link"></a>
             </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 import lightLogoutButton from "../../assets/logout-icon-light.svg";
 import darkLogoutButton from "../../assets/logout-icon-dark.svg";
+import lightHamburgerIcon from "../../assets/mobile-menu-light.svg";
+import darkHamburgerIcon from "../../assets/mobile-menu-dark.svg";
 import './Navigation.css';
 
 function Navigation({ handleModalOpen, handleLogout }) {
     const { currentUser, isLoggedIn, identifyLocation } = useContext(CurrentUserContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const currentUserFirstName = currentUser?.name ? currentUser.name.split(" ")[0] : "";
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     return(
         <nav className={`nav ${identifyLocation === "/saved-news" ? "nav_dark" : ""}`}>
@@ -18,28 +23,33 @@ function Navigation({ handleModalOpen, handleLogout }) {
                     <span className="nav__logo-text">NewsExplorer</span>
                 </p>
             </Link>
-            {isLoggedIn ? (
-                <>
-                    <div className="nav__links">
-                        <Link to="/">
-                            <button className={`nav__home-button ${identifyLocation === "/" ? "selected" : ""}`}>Home</button>
-                        </Link>
-                        <Link to="/saved-news">
-                            <button className={`nav__home-button ${identifyLocation === "/saved-news" ? "selected" : ""}`}>Saved articles</button>
-                        </Link>
-                        <button onClick={handleLogout} className="nav__user-button">{currentUserFirstName}<img src={identifyLocation === "/saved-news" ? darkLogoutButton : lightLogoutButton} alt="logout icon" className="nav__logout-icon" /></button>
-                    </div>
-                </>
-            ): (
-                <>
-                    <div className="nav__links">
-                        <Link to="/">
-                            <button className={`nav__home-button ${identifyLocation === "/" ? "selected" : ""}`}>Home</button>
-                        </Link>
-                        <button onClick={() => handleModalOpen("login")} className="nav__signin-button">Sign in</button>
-                    </div>
-                </>
-            )}
+            <div className="nav__links-container">
+                {isLoggedIn ? (
+                    <>
+                        <div className="nav__links">
+                            <Link to="/">
+                                <button className={`nav__home-button ${identifyLocation === "/" ? "selected" : ""}`}>Home</button>
+                            </Link>
+                            <Link to="/saved-news">
+                                <button className={`nav__home-button ${identifyLocation === "/saved-news" ? "selected" : ""}`}>Saved articles</button>
+                            </Link>
+                            <button onClick={handleLogout} className="nav__user-button">{currentUserFirstName}<img src={identifyLocation === "/saved-news" ? darkLogoutButton : lightLogoutButton} alt="logout icon" className="nav__logout-icon" /></button>
+                        </div>
+                    </>
+                ): (
+                    <>
+                        <div className="nav__links">
+                            <Link to="/">
+                                <button className={`nav__home-button ${identifyLocation === "/" ? "selected" : ""}`}>Home</button>
+                            </Link>
+                            <button onClick={() => handleModalOpen("login")} className="nav__signin-button">Sign in</button>
+                        </div>
+                    </>
+                )}
+                <button className="nav__hamburger-menu" onClick={toggleMenu}>
+                    <img src={identifyLocation === "/saved-news" ? darkHamburgerIcon : lightHamburgerIcon} alt="menu" />
+                </button>
+            </div>
         </nav>
     )
 }
