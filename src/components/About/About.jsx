2c14a7ea9f988afd1b5095cdd63145e3d9e10373
BrandonRogers <ber64111@gmail.com>
2024-12-18 08:22:41 -0700

    fix: BEM conventions and media queries

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,27 @@
 import './About.css';
 import headshot from "../../assets/headshot.jpeg";
 
-function About(){
-    return(
+function About() {
+    return (
         <section className="about">
             <img src={headshot} alt="headshot" className="about__image" />
             <div className="about__details">
                 <h2 className="about__title">About the author</h2>
                 <p className="about__desc">
-                    Hey, I&apos;m Brandon Rogers, a full-stack software developer participating in the TripleTen Software Engineering Bootcamp. <br/><br/>
-                    Right now, I&apos;m an SEO Specialist at Amsive, a full-time robotics major at Metropolitan State University of Denver, and co-founder of an app called Inscription.<br/><br/>
-                    I&apos;m currently proficient in development technologies that include HTML, CSS, JavaScript, NodeJS, React, Vue, Express, MongoDB, and more, most of which I have been able to learn throughout the TripleTen Bootcamp.<br/><br/>
+                    Hey, I&apos;m Brandon Rogers, a full-stack software developer participating in the TripleTen Software Engineering Bootcamp.
+                </p>
+                <p className="about__desc">
+                    Right now, I&apos;m an SEO Specialist at Amsive, a full-time robotics major at Metropolitan State University of Denver, and co-founder of an app called Inscription.
+                </p>
+                <p className="about__desc">
+                    I&apos;m currently proficient in development technologies that include HTML, CSS, JavaScript, NodeJS, React, Vue, Express, MongoDB, and more, most of which I have been able to learn throughout the TripleTen Bootcamp.
+                </p>
+                <p className="about__desc">
                     As the last sprint in the TripleTen Software Engineering Bootcamp, I chose to build this NewsExplorer application, so I hope you enjoy using it!
                 </p>
             </div>
         </section>
-    )
+    );
 }
 
 export default About;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,26 @@ function Footer() {
     return (
         <footer className="footer">
             <p className="footer__details">&copy;2024 Supersite, Powered by News API</p>
-            <div className="footer__links">
-                <div className="footer__navigation-links">
-                    <a href="/" className="footer__home-link">Home</a>
-                    <a href="https://tripleten.com/" className="footer__tripleten-link">TripleTen</a>
-                </div>
-                <a href="https://github.com/tripleten-com" className="footer__github-link"></a>
-                <a href="https://www.facebook.com/tripleten.tech/" className="footer__facebook-link"></a>
-            </div>
+            <nav className="footer__nav">
+                <ul className="footer__nav-links">
+                    <div className="footer__site-links">
+                        <li className="footer__item">
+                            <a href="/" className="footer__link footer__link_type_home">Home</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="https://tripleten.com/" className="footer__link footer__link_type_tripleten" target="_blank" rel="noopener noreferrer">TripleTen</a>
+                        </li>
+                    </div>
+                    <div className="footer__social-links">
+                        <li className="footer__item">
+                            <a href="https://github.com/tripleten-com" className="footer__link footer__link_type_github" target="_blank" rel="noopener noreferrer"></a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="https://www.facebook.com/tripleten.tech/" className="footer__link footer__link_type_facebook" target="_blank" rel="noopener noreferrer"></a>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
         </footer>
     );
 }
