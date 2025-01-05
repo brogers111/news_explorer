b8091c73359a27a98df0e44adfbae639d0a0a4f2
BrandonRogers <ber64111@gmail.com>
2025-01-04 20:20:10 -0700

    Prep for build

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-// export const NEWS_API_KEY = process.env.REACT_APP_API_KEY || "";
-
-export const NEWS_API_KEY = "426f463ec70c420a91874a51172acf63";
+export const NEWS_API_KEY = process.env.REACT_APP_API_KEY || "";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
