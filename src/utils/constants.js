90f454355f5c7676cbc5b2800ae355294f3456e0
BrandonRogers <ber64111@gmail.com>
2025-01-04 19:48:34 -0700

    Update BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,4 @@ export const NEWS_API_KEY = "426f463ec70c420a91874a51172acf63";
 //     ? "https://api.news-explorer.h0stname.net"
 //     : "http://localhost:3000";
 
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "http://localhost:3002";
