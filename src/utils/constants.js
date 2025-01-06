a4fe5057acd58eaac645f24e93558ede606669b6
BrandonRogers <ber64111@gmail.com>
2025-01-06 14:02:11 -0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const NEWS_API_KEY = process.env.REACT_APP_API_KEY || "";
+export const NEWS_API_KEY = import.meta.env.VITE_APP_API_KEY || "";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
